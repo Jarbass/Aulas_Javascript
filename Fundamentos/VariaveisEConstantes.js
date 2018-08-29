2bd00cde72404ee2e4275e5e564934c68ee0548f
Jardel <prof_jardel@hotmail.com>
2018-08-29 19:53:57 -0300

    Aula 04

---
+++ Fundamentos/VariaveisEConstantes.js
@@ -0,0 +1,24 @@
+var a = 3;
+let b = 4;
+
+var a = 30;
+    b = 40;
+
+console.log(a, b);
+
+a = 300;
+b = 200;
+
+console.log (a, b);
+const c = 5;
+
+console.log(c);
+
+//testando calculo
+
+let a = 2;
+let b = 5;
+
+console.log(a+b);
+
+//EndTeste: Funciona !
