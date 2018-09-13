4c4e68d94107365744de7d335e2b0ee2caae1455
Jardel <prof_jardel@hotmail.com>
2018-09-13 19:56:53 -0300

    Aprendendo algumas diferenças de Var e Let

---
+++ Fundamentos/usandoLet1.js
@@ -0,0 +1,11 @@
+var numero = 1
+{
+    let numero = 2
+    console.log("dentro =", numero)
+}
+
+console.log("fora =", numero)
+
+/* Var tem escopo global e dentro da Function
+    Já o Let tem escopo global, dentro da Function e por bloco.
+    No exemplo acima o let só poder ser visto dentro dos {}*/
---
+++ Fundamentos/usandoVar1.js
@@ -0,0 +1,14 @@
+{{{{ var sera = "Será???"}}}}
+
+console.log(sera)
+
+function teste () {
+    var local = 123
+}
+
+teste()
+console.log(local)
+
+//Evitar var de escopo global
+//var tem dois escopos: Global - Visível para todos
+//e var dentro de uma function - Só é visível dentro da function
