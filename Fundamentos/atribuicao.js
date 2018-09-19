9437671f31704300337024316cc7562dfa6c416d
Jardel <prof_jardel@hotmail.com>
2018-09-19 19:13:34 -0300

    Alguns sinais de atribuição

---
+++ Fundamentos/atribuicao.js
@@ -0,0 +1,22 @@
+const a = 7
+let b = 3
+
+//Junto com a adição, faz a atribuição, acrescentando valor de 7 em b
+b += a
+console.log(b)
+
+//Diminui o valor de b. b=b-4
+b -= 4
+console.log(b)
+
+//Multiplica o valor de b. b= b*2
+b *= 2
+console.log(b)
+
+//Dividi o valor de b. b= b / 2
+b /= 2
+console.log(b)
+
+//Módulo do valor de b. b= b % 2
+b %= 2
+console.log (b)
