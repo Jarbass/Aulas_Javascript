55498737924410c61e9c8e5cc93a899a9213bc73
Jardel <prof_jardel@hotmail.com>
2018-10-24 20:57:10 -0300

    Conceitos e Exercícios com Objetos em JS

---
+++ Curso/Objeto/oo.js
@@ -0,0 +1,23 @@
+//Código não executável !!!
+
+//Procedural
+
+processamento(valor1, valor2, valor3);
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    precossamento() {
+        //...
+    }
+}
+
+objeto.processamento(); //Foco passoua ser o objeto
+
+//Princípios importantes:
+//1. Abstração 
+//2. Encapsulamento
+//3. Herança
+//4. Polimorfismo
