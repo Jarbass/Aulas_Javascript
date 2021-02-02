6f00eaf6f850573677849e001b9a8caa650a1fae
Jardel <prof_jardel@hotmail.com>
2021-02-01 22:59:10 -0300

    Estudando DOM

---
+++ DOM/atividade-01.js
@@ -0,0 +1,16 @@
+//Retorne o URL da página atual utilizando o objeto window
+
+console.log(window.location.href);
+
+//Selecione o primeiro elemento da página que possua a classe ativo
+
+const elementAtivo = document.querySelector('.ativo');
+console.log(elementAtivo);
+
+//Retorne a linguagem do navegador
+
+console.log(window.navigator.language);
+
+//Retorne a largura da janela
+
+console.log(window.innerWidth);
