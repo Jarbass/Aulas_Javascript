757c94dd3a6c69e1519ac3e90748b6c1f791af6a
Jardel <prof_jardel@hotmail.com>
2021-02-08 19:33:34 -0300

    Estudando Prototype

---
+++ Objetos/prototype.js
@@ -0,0 +1,37 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne o nome completo da pessoa
+
+function Pessoa(nome, sobrenome, idade) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    idade = idade;
+}
+
+Pessoa.prototype.nomeCompleto = function() {
+    return `${this.nome} ${this.sobrenome}`;
+}
+
+const pessoa = new Pessoa("Jardel", "Lima", 22);
+
+
+
+// Liste os métodos acessados por dados criados com NodeList, HTMLColletion, Document
+
+Object.getOwnPropertyNames(NodeList.prototype);
+Object.getOwnPropertyNames(HTMLCollection.prototype);
+Object.getOwnPropertyNames(Document.prototype);
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector("li");
+
+li; // HTMLLIElement
+li.click; // Function
+li.innerHTML; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; // Number
+li.click(); // Undefined
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; // Tipo de dado String especificando o dado, por exemplo: "Boolean".
