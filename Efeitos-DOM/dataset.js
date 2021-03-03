2957ad819083f348767f990fd7b5bb515e162e54
Jardel <prof_jardel@hotmail.com>
2021-03-03 11:44:27 -0300

    Estudando Efeitos DOM

---
+++ Efeitos-DOM/dataset.js
@@ -0,0 +1,9 @@
+// Adicione um atributo data-anime="show-down" e
+// data-anime="show-right" a todos as section's
+// com descrição dos animais.
+
+// No CSS faça com que show-down anime de cima para baixo
+// e show-right continue com a mesma animação da esquerda para a direita
+
+// Substitua todas as classes js- por data atributes
+
